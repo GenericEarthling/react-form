300cbe7c1e26771ab08bb830b29e3b1dfaa5aa3a
GenericEarthling <sharon.tender@icloud.com>
2018-09-19 22:14:03 -0400

    Added preliminary form

--- src/App.js
+++ src/App.js
@@ -8,11 +8,22 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">A React Form</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Enter your credentials.
         </p>
+        <div className="formbox">
+        <form>
+          <label for="humanName">Name:</label>
+          <input type="text" name="humanName" id="humanName"></input>
+          <label for="email">E-mail:</label>
+          <input type="text" name="email" id="email"></input>
+          <label for="comments">Comments:</label>
+          <textarea name="comments" id="comments" rows="2" cols="20"></textarea>
+          <input id="submitbtn" type="submit" value="Submit"></input>
+        </form>
+        </div>
       </div>
     );
   }
