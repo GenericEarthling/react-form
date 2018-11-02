d5e96b8ee3a3e2f9284d34d5b787c09cb8e93dda
GenericEarthling <sharon.tender@icloud.com>
2018-11-02 16:53:21 -0400

    updated merge version from 1.2.0 to 1.2.1 in .json file (2 places+link)

--- src/App.js
+++ src/App.js
@@ -3,27 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {userName: '', 
+                  userEmail: ''};
+  }
+  // this will save the value to state when changed
+  inputChanged = event => {
+    this.setState({[event.target.name]:event.target.value});
+  }
+  handleSubmit = event => {
+    alert(`Are you really ${this.state.userName} from ${this.state.userEmail}?`);
+    event.preventDefault();
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">A React Form</h1>
-        </header>
-        <p className="App-intro">
-          Enter your credentials.
-        </p>
-        <div className="formbox">
-        <form>
-          <label for="humanName">Name:</label>
-          <input type="text" name="humanName" id="humanName"></input>
-          <label for="email">E-mail:</label>
-          <input type="text" name="email" id="email"></input>
-          <label for="comments">Comments:</label>
-          <textarea name="comments" id="comments" rows="2" cols="20"></textarea>
-          <input id="submitbtn" type="submit" value="Submit"></input>
-        </form>
-        </div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">A React Form</h1>
+          </header>
+          <p className="App-intro">
+            Enter your credentials.
+          </p>
+          <div className="formbox">
+          <form onSubmit={this.handleSubmit}>
+            <label for="userName">Name:</label>
+            <input onChange={this.inputChanged} value={this.state.userName} type="text" name="userName" id="userName"></input>
+            <label for="userEmail">E-mail:</label>
+            <input onChange={this.inputChanged} value={this.state.userEmail} type="text" name="userEmail" id="userEmail"></input>
+            <input id="submitbtn" type="submit" value="Submit"></input>
+          </form>
+          </div>
       </div>
     );
   }
